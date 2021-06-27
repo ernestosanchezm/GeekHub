248982056e285ce862b03fae6f5c52963b3e2d0d
Arvin <45604781+NivraLeak@users.noreply.github.com>
2021-06-26 21:16:58 -0500

    Agregando a la rama principal

---
+++ GeekHub/Presentation/frmMainView.Designer.cs
@@ -0,0 +1,135 @@
+﻿
+namespace GeekHub.Presentation
+{
+    partial class frmMainView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.flowLayoutPanelProducts = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.bttLogOut = new System.Windows.Forms.Button();
+            this.bttOptions = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::GeekHub.Properties.Resources.user;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(55, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // flowLayoutPanelProducts
+            // 
+            this.flowLayoutPanelProducts.AutoScroll = true;
+            this.flowLayoutPanelProducts.Location = new System.Drawing.Point(12, 132);
+            this.flowLayoutPanelProducts.Name = "flowLayoutPanelProducts";
+            this.flowLayoutPanelProducts.Size = new System.Drawing.Size(776, 306);
+            this.flowLayoutPanelProducts.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.bttLogOut);
+            this.panel1.Controls.Add(this.bttOptions);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(12, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(776, 80);
+            this.panel1.TabIndex = 2;
+            // 
+            // bttLogOut
+            // 
+            this.bttLogOut.Location = new System.Drawing.Point(619, 23);
+            this.bttLogOut.Name = "bttLogOut";
+            this.bttLogOut.Size = new System.Drawing.Size(141, 23);
+            this.bttLogOut.TabIndex = 3;
+            this.bttLogOut.Text = "Cerrar sesion";
+            this.bttLogOut.UseVisualStyleBackColor = true;
+            // 
+            // bttOptions
+            // 
+            this.bttOptions.Location = new System.Drawing.Point(531, 23);
+            this.bttOptions.Name = "bttOptions";
+            this.bttOptions.Size = new System.Drawing.Size(82, 23);
+            this.bttOptions.TabIndex = 2;
+            this.bttOptions.Text = "Opciones";
+            this.bttOptions.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(93, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(401, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Products";
+            // 
+            // frmMainView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.flowLayoutPanelProducts);
+            this.Name = "frmMainView";
+            this.Text = "frmMainView";
+            this.Load += new System.EventHandler(this.frmMainView_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanelProducts;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button bttLogOut;
+        private System.Windows.Forms.Button bttOptions;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ GeekHub/Presentation/frmViewProductsDetail.Designer.cs
@@ -0,0 +1,192 @@
+﻿
+namespace GeekHub.Presentation
+{
+    partial class frmViewProductsDetail
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanelProduct = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.lblFilterCategory = new System.Windows.Forms.Label();
+            this.flowLayoutPanelOrden = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutFiltrosCategory = new System.Windows.Forms.FlowLayoutPanel();
+            this.lblFiltros = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanelProduct
+            // 
+            this.flowLayoutPanelProduct.AutoScroll = true;
+            this.flowLayoutPanelProduct.Location = new System.Drawing.Point(9, 121);
+            this.flowLayoutPanelProduct.Margin = new System.Windows.Forms.Padding(4);
+            this.flowLayoutPanelProduct.Name = "flowLayoutPanelProduct";
+            this.flowLayoutPanelProduct.Size = new System.Drawing.Size(644, 325);
+            this.flowLayoutPanelProduct.TabIndex = 18;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 17);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Products Detail";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(133, 26);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(431, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(807, 74);
+            this.panel1.TabIndex = 13;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(664, 26);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(124, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Cerrar session";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(571, 26);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(87, 23);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "opciones";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // lblFilterCategory
+            // 
+            this.lblFilterCategory.AutoSize = true;
+            this.lblFilterCategory.Location = new System.Drawing.Point(663, 216);
+            this.lblFilterCategory.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblFilterCategory.Name = "lblFilterCategory";
+            this.lblFilterCategory.Size = new System.Drawing.Size(69, 17);
+            this.lblFilterCategory.TabIndex = 17;
+            this.lblFilterCategory.Text = "Categoria";
+            // 
+            // flowLayoutPanelOrden
+            // 
+            this.flowLayoutPanelOrden.AutoScroll = true;
+            this.flowLayoutPanelOrden.Location = new System.Drawing.Point(663, 121);
+            this.flowLayoutPanelOrden.Margin = new System.Windows.Forms.Padding(4);
+            this.flowLayoutPanelOrden.Name = "flowLayoutPanelOrden";
+            this.flowLayoutPanelOrden.Size = new System.Drawing.Size(141, 91);
+            this.flowLayoutPanelOrden.TabIndex = 16;
+            // 
+            // flowLayoutFiltrosCategory
+            // 
+            this.flowLayoutFiltrosCategory.AutoScroll = true;
+            this.flowLayoutFiltrosCategory.Location = new System.Drawing.Point(663, 233);
+            this.flowLayoutFiltrosCategory.Margin = new System.Windows.Forms.Padding(4);
+            this.flowLayoutFiltrosCategory.Name = "flowLayoutFiltrosCategory";
+            this.flowLayoutFiltrosCategory.Size = new System.Drawing.Size(141, 207);
+            this.flowLayoutFiltrosCategory.TabIndex = 15;
+            // 
+            // lblFiltros
+            // 
+            this.lblFiltros.AutoSize = true;
+            this.lblFiltros.Location = new System.Drawing.Point(716, 101);
+            this.lblFiltros.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblFiltros.Name = "lblFiltros";
+            this.lblFiltros.Size = new System.Drawing.Size(46, 17);
+            this.lblFiltros.TabIndex = 14;
+            this.lblFiltros.Text = "Filtros";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::GeekHub.Properties.Resources.user;
+            this.pictureBox2.Location = new System.Drawing.Point(13, 14);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(85, 50);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // frmViewProductsDetail
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(807, 451);
+            this.Controls.Add(this.flowLayoutPanelProduct);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lblFilterCategory);
+            this.Controls.Add(this.flowLayoutPanelOrden);
+            this.Controls.Add(this.flowLayoutFiltrosCategory);
+            this.Controls.Add(this.lblFiltros);
+            this.Name = "frmViewProductsDetail";
+            this.Text = "ViewProductsDetail";
+            this.Load += new System.EventHandler(this.frmViewProductsDetail_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanelProduct;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label lblFilterCategory;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanelOrden;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutFiltrosCategory;
+        private System.Windows.Forms.Label lblFiltros;
+    }
+}
