689bd3f358a860699fbad1afeb8bfdd9ba0e6431
ernesto_sanchez_matos <u201713827@upc.edu.pe>
2021-06-26 22:04:56 -0500

    __

--- GeekHub/Presentation/frmSellerMain.cs
+++ GeekHub/Presentation/frmSellerMain.cs
@@ -13,43 +13,37 @@ namespace GeekHub.Presentation
 {
     public partial class frmSellerMain : Form
     {
-        public Form1()
-        {
-            this.listItems = new List<ListItem>();
-            for (int i = 0; i < 20; i++)
-            {
-                this.listItems.Add(new ListItem());
-            }
+        public frmSellerMain()
+        {           
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void frmSellerMain_Load(object sender, EventArgs e)
         {
 
-            populateItems();
         }
 
-        private void populateItems()
-        {
-            ListItem[] items = new ListItem[20];
-            //loop through each item
-            for (int i = 0; i < listItems.Count; i++)
-            {
-                listItems[i].NameProduct = "name";
-                if(i%2 == 0)
-                {
-                    listItems[i].ImageProduct = Resources.ropa1;
-                    listItems[i].CategoryProduct = "Category: Ropa";
-                }
-                else
-                {
-                    listItems[i].ImageProduct = Resources.comics1;
-                    listItems[i].CategoryProduct = "Category: Comics";
-                }
-                flowLayoutPanel1.Controls.Add(listItems[i]);
+        //private void populateItems()
+        //{
+        //    ListItem[] items = new ListItem[20];
+        //    //loop through each item
+        //    for (int i = 0; i < listItems.Count; i++)
+        //    {
+        //        listItems[i].NameProduct = "name";
+        //        if(i%2 == 0)
+        //        {
+        //            listItems[i].ImageProduct = Resources.ropa1;
+        //            listItems[i].CategoryProduct = "Category: Ropa";
+        //        }
+        //        else
+        //        {
+        //            listItems[i].ImageProduct = Resources.comics1;
+        //            listItems[i].CategoryProduct = "Category: Comics";
+        //        }
+        //        flowLayoutPanel1.Controls.Add(listItems[i]);
 
-            }
+        //    }
 
-        }
+        //}
     }
 }
